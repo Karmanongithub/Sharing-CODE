5ff87e385d3e29498ffd735e80072a2275931ddb
Karmanongithub <92850381+Karmanongithub@users.noreply.github.com>
2021-10-20 13:50:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+
+function setup() {
+  createCanvas(500,500);
+  var box= createSprite(200,200,25,25)
+box.shapeColor= "lightgreen"
+var box2= createSprite(300,300,25,25)
+box2.shapeColor="lightblue"
+}
+
+function draw() 
+{
+  background(30);
+
+
+  drawSprites()
+}
+
+
+
+
